4a941fdfa02e737dc599cfc2006fd27cd447393e
vaclavbohac <bohac.v@gmail.com>
2017-10-04 19:16:42 +0200

    Statement - Segment - Article migration

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class Segment < ApplicationRecord
+  TYPE_TEXT = "text"
+  TYPE_STATEMENTS_SET = "statements_set"
+
   has_many :statements, through: SegmentHasStatement
   has_many :segments, through: ArticleHasSegment
+
+  def is_text?
+    segment_type == Segment::TYPE_TEXT
+  end
 end
--- lib/migrate/statement_migration.rb
+++ lib/migrate/statement_migration.rb
@@ -9,11 +9,18 @@ class StatementMigration
   def initialize(connection)
     self.connection = connection
     @tester = DuplicationTester.new
+    @segment_cache = Hash.new
   end
 
   def perform
     old_statements = self.connection.query("SELECT * FROM vyrok")
 
+    migrate_statements(old_statements)
+    migrate_assessments(old_statements)
+    migrate_segments(old_statements)
+  end
+
+  def migrate_statements(old_statements)
     keys = [
       :id,
       :source_id,
@@ -43,6 +50,102 @@ class StatementMigration
                      Time.now,
                      Time.now
                    ])
+      end
+    end
+  end
+
+  def evaluation_status
+    {
+        -3 => Assessment::STATUS_REMOVED,
+        -2 => Assessment::STATUS_TO_BE_EVALUATED,
+        -1 => Assessment::STATUS_TO_BE_CHECKED_BY_SUPERVISOR,
+         0 => Assessment::STATUS_CORRECT,
+         1 => Assessment::STATUS_CORRECT
+    }
+  end
+
+
+  def migrate_assessments(old_statements)
+    keys = [
+      :statement_id,
+      :explanation,
+      :veracity_id,
+      :evaluation_status,
+      :user_id,
+      :evaluated_at,
+      :created_at,
+      :updated_at
+    ]
+
+    Assessment.bulk_insert(*keys) do |worker|
+      old_statements.each do |old_statement|
+        next unless old_statement["status"] >= -1
+
+        veracity_id = nil
+        if old_statement["id_pravdivostna_hodnota"]
+          veracity_id = old_statement["id_pravdivostna_hodnota"]
+        end
+
+        worker.add([
+          old_statement["id"],
+          old_statement["odovodnenie"],
+          veracity_id,
+          evaluation_status[old_statement["status"]],
+          old_statement["id_user"],
+          old_statement["timestamp"],
+          Time.now,
+          Time.now
+        ])
+      end
+    end
+  end
+
+  def dependency_to_segment(source_id)
+    return @segment_cache[source_id] if @segment_cache[source_id]
+
+    article_has_segment = ArticleHasSegment.find_by(article: source_id)
+
+    if article_has_segment
+      @segment_cache[source_id] = article_has_segment.segment
+    else
+      article = Article.find(source_id)
+
+      raise "Article #{source_id} not found" unless article
+
+      segment = Segment.create!(
+        segment_type: Segment::TYPE_STATEMENTS_SET
+      )
+
+      @segment_cache[source_id] = segment
+
+      ArticleHasSegment.create!(
+        article: article,
+        segment: segment,
+        order: 1
+      )
+
+      segment
+    end
+  end
+
+  def migrate_segments(old_statements)
+    keys = [
+      :segment_id,
+      :statement_id,
+      :created_at,
+      :updated_at
+    ]
+
+    SegmentHasStatement.bulk_insert(*keys) do |worker|
+      old_statements.each do |old_statement|
+        segment = dependency_to_segment(old_statement["id_diskusia"])
+
+        worker.add([
+          segment.id,
+          old_statement["id"],
+          Time.now,
+          Time.now
+        ])
       end
     end
   end
