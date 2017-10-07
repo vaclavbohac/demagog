5288cc142a466b02540ff75449398293587ea513
vaclavbohac <bohac.v@gmail.com>
2017-10-07 10:19:56 +0200

    Add czech locale & translate paging

---
+++ config/initializers/locale.rb
@@ -0,0 +1,8 @@
+# Where the I18n library should search for translation files
+# I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+
+# Whitelist locales available for the application
+I18n.available_locales = [:en, :cs]
+
+# Set default locale to czech
+I18n.default_locale = :cs
