6b2aff49083f5ffee1848ef145f652d5b96db76f
vaclavbohac <bohac.v@gmail.com>
2017-10-07 19:51:57 +0200

    Caching of homepage and article for 1 hour

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -3,5 +3,12 @@
 class ArticleController < ApplicationController
   def index
     @article = Article.find(params[:id])
+
+    expires_in 1.hour, public: true
+    if stale? @article, public: true
+      respond_to do |format|
+        format.html
+      end
+    end
   end
 end
--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -8,5 +8,7 @@ class HomepageController < ApplicationController
       .where(published: true)
       .order(published_at: :desc)
       .page(params[:page])
+
+    expires_in 1.hour, public: true
   end
 end
