a2d13b99bb941dce86ffad9586258671f91fe9b3
vaclavbohac <bohac.v@gmail.com>
2017-10-22 16:18:44 +0200

    Use demagogimage service for legacy images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,17 @@
 
 module ApplicationHelper
   def speaker_portrait(attachment)
-    "http://demagog.cz/data/politik/t/#{attachment.file}"
+    "#{server}/data/politik/t/#{attachment.file}"
   end
 
   def article_illustration(attachment, article_type = "default")
     folder = article_type === "static" ? "pages" : "diskusia"
 
-    "http://demagog.cz/data/#{folder}/s/#{attachment.file}"
+    "#{server}/data/#{folder}/s/#{attachment.file}"
   end
+
+  private
+    def server
+      ENV["DEMAGOG_IMAGE_SERVICE_URL"]
+    end
 end
