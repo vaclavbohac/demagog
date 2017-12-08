92b8d861bd038744315e3dcada6bed1e313fa1d8
Vaclav Bohac <bohac.v@gmail.com>
2017-12-08 18:56:11 +0100

    Enable GraphiQL on production

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
 
   post "/graphql", to: "graphql#execute"
   concern :paginatable do
