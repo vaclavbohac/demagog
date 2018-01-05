907211b12b57a43ff4071c9eac5e9142e495e003
vaclavbohac <bohac.v@gmail.com>
2018-01-05 21:19:35 +0100

    Add tests of Body#current_members

--- test/models/body_test.rb
+++ test/models/body_test.rb
@@ -3,7 +3,13 @@
 require "test_helper"
 
 class BodyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "current members" do
+    party_a = bodies(:body_a)
+
+    # Total number of membership is 2
+    assert_equal 2, party_a.memberships.count
+
+    # Yet there is only single current member
+    assert_equal 1, party_a.current_members.count
+  end
 end
