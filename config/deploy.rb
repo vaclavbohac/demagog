9b4c9dd3b294c03cd694891ed1afb5c3c68271a8
Vaclav Bohac <bohac.v@gmail.com>
2018-04-26 09:57:04 +0200

    Share log and cache directories between releases (#73)

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :unicorn_config_path, "/etc/unicorn.conf"
 set :linked_files, %w{config/database.yml config/unicorn/production.rb .env}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-set :linked_dirs,  %w{tmp/pids}
+set :linked_dirs,  %w{log tmp/pids tmp/cache}
 
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
