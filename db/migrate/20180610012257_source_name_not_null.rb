02c0f73e2db879d6a15bba4b02634b46cc47527b
Vaclav Bohac <bohac.v@gmail.com>
2018-06-12 21:35:38 +0200

    Fix source migration

--- db/migrate/20180610012257_source_name_not_null.rb
+++ db/migrate/20180610012257_source_name_not_null.rb
@@ -1,5 +1,7 @@
 class SourceNameNotNull < ActiveRecord::Migration[5.2]
   def up
+    Source.where(name: nil).update_all(name: "")
+
     change_column :sources, :name, :string, null: false
   end
 
