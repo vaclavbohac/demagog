bff6bf47245e7ee2c0d41a4dcd80e80e314b7d8b
Jan Vlcek <vlki@vlki.cz>
2018-06-28 21:00:36 +1200

    Fix roles migration to work as usual

--- db/migrate/20180625071730_change_roles_to_have_key_and_name.rb
+++ db/migrate/20180625071730_change_roles_to_have_key_and_name.rb
@@ -3,10 +3,19 @@ class ChangeRolesToHaveKeyAndName < ActiveRecord::Migration[5.2]
     rename_column :roles, :name, :key
     add_column :roles, :name, :string
 
-    Role.find_by(key: "admin").update!(name: "Administrátor")
-    Role.find_by(key: "expert").update!(name: "Expert")
-    Role.find_by(key: "stazista").update!(key: "intern", name: "Stážista")
-    Role.create!(key: "social_media_manager", name: "Síťař")
-    Role.create!(key: "proofreader", name: "Korektor")
+    create_or_update_role("admin", key: "admin", name:  "Administrátor")
+    create_or_update_role("expert", key: "expert", name:  "Expert")
+    create_or_update_role("stazista", key: "intern", name:  "Stážista")
+    create_or_update_role("", key: "social_media_manager", name:  "Síťař")
+    create_or_update_role("", key: "proofreader", name:  "Korektor")
+  end
+
+  def create_or_update_role(old_key, role)
+    existing = Role.find_by(key: old_key)
+    if existing.nil?
+      Role.create!(role)
+    else
+      existing.update!(role)
+    end
   end
 end
