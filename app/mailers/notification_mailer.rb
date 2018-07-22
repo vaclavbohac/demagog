64f24e7c2ccec7c673633b5a51cb6b2a927442e8
Jan Vlcek <vlki@vlki.cz>
2018-07-22 21:51:40 +0200

    Better email headers

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 class NotificationMailer < ApplicationMailer
+  default from: "Upozornění z administace Demagog.cz <noreply@demagog.cz>"
+
   def notification_email
     @notification = params[:notification]
     @host = ActionMailer::Base.default_url_options[:host]
-    mail(to: @notification.recipient.email, subject: @notification.content)
+
+    recipient = @notification.recipient
+    email_with_name = "#{@recipient.first_name} #{recipient.last_name} <#{recipient.email}>"
+
+    mail(to: email_with_name, subject: @notification.content)
   end
 end
