af1de55a8885e1e04141881bed7ce65379057a4e
Vaclav Bohac <bohac.v@gmail.com>
2019-01-28 21:47:54 +0100

    Make elasticsearch host configurable

---
+++ config/initializers/elasticsearch.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+config = {
+  host: "http://localhost:9200/",
+  transport_options: {
+    request: { timeout: 5 }
+  }
+}
+
+if File.exists?("config/elasticsearch.yml")
+  config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].symbolize_keys)
+end
+
+Elasticsearch::Model.client = Elasticsearch::Client.new(config)
