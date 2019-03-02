8dc4be28b53ae203ff81d5f7c8b76f9dbbd0852e
Jan Vlcek <vlki@vlki.cz>
2019-03-02 14:27:20 +0100

    Remove unnecessary code

--- app/javascript/admin/index.tsx
+++ app/javascript/admin/index.tsx
@@ -31,16 +31,6 @@ import * as ActiveStorage from 'activestorage';
 
 ActiveStorage.start();
 
-// import { Classes, FormGroup } from '@blueprintjs/core';
-
-// We use the required label the other way around: to mark fields which
-// are NOT required
-// FormGroup.DEFAULT_REQUIRED_CONTENT = (
-//   <small className={Classes.TEXT_MUTED} style={{ paddingLeft: 7 }}>
-//     nepovinné
-//   </small>
-// );
-
 import App from './App';
 import rootEpic from './epics';
 import rootReducer from './reducers';
