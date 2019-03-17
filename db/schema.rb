3dbea9517822089e214ac17118c51a36b47a5104
Jan Vlcek <vlki@vlki.cz>
2019-03-17 19:39:55 +0100

    Add API docs page (#346)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_03_191709) do
+ActiveRecord::Schema.define(version: 2019_03_17_182705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
