b90db68048ad9f34fe51aaa5b10f4a27235a0c12
Jan Vlcek <vlki@vlki.cz>
2019-05-12 13:59:24 +0200

    Add promises banner to homepage, update cover story layout (#385)

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -5,8 +5,13 @@ class HomepageController < ApplicationController
     unless params[:page].present?
       @cover_story = Article.cover_story
       @interesting_statements = Statement.interesting_statements
+
+      # Preview only for users signed in to admin. Remove when launching.
+      @show_promises = user_signed_in?
+      @promises_stats = get_promises_stats
     else
       @page_number = params[:page]
+      @show_promises = false
     end
 
     @articles = Article
@@ -19,4 +24,18 @@ class HomepageController < ApplicationController
     # TODO: revisit cache headers and do properly
     # expires_in 1.hour, public: true
   end
+
+  private
+    def get_promises_stats
+      keys = [PromiseRating::FULFILLED, PromiseRating::IN_PROGRESS, PromiseRating::BROKEN, PromiseRating::STALLED]
+
+      statements = Statement
+        .where(source_id: [562])
+        .where(assessments: {
+          evaluation_status: Assessment::STATUS_APPROVED,
+        })
+        .includes(:assessment, assessment: :promise_rating)
+
+      keys.map { |key| [key, statements.where(assessments: { promise_ratings: { key: key } }).count] }.to_h
+    end
 end
