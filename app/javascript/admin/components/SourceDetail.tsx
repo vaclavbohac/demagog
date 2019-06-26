7113c14cd8d33c4b5215e8c60ddd298495756d96
Vaclav Bohac <bohac.v@gmail.com>
2019-06-26 23:30:33 +0200

    Add special statement filter (#410)

--- app/javascript/admin/components/SourceDetail.tsx
+++ app/javascript/admin/components/SourceDetail.tsx
@@ -438,6 +438,13 @@ class SourceDetail extends React.Component<IProps, IState> {
 
           const statementsToDisplay = data.statements.filter((statement) => {
             if (statementsFilter !== null) {
+              if (statementsFilter.field === 'verifiedAndUnpublished') {
+                return (
+                  statement.assessment.evaluationStatus === ASSESSMENT_STATUS_APPROVED &&
+                  statement.published === false
+                );
+              }
+
               return get(statement, statementsFilter.field, null) === statementsFilter.value;
             } else {
               return true;
@@ -536,6 +543,22 @@ class SourceDetail extends React.Component<IProps, IState> {
                         onClick={this.onStatementsFilterClick('published', option.value)}
                       />
                     ))}
+
+                    <MenuItem
+                      text={`Nezveřejněné, schválené (${
+                        data.statements.filter(
+                          (statement) =>
+                            statement.assessment.evaluationStatus === ASSESSMENT_STATUS_APPROVED &&
+                            statement.published === false,
+                        ).length
+                      })`}
+                      active={
+                        statementsFilter !== null &&
+                        statementsFilter.field === 'verifiedAndUnpublished' &&
+                        statementsFilter.value === true
+                      }
+                      onClick={this.onStatementsFilterClick('verifiedAndUnpublished', true)}
+                    />
 
                     <MenuDivider title="Filtrovat dle ověřovatele" />
                     {evaluatorFilterOptions.map((option) => (
