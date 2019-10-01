9064e2847463b3548569e976393e251293af48f6
Jan Vlcek <vlki@vlki.cz>
2019-10-01 14:41:12 +0200

    Fix api docs which need Prism and hide IFCN till we have new non-expired badge code (#447)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 
-Rails.application.config.assets.precompile += %w[admin.css]
+# prism.css & prism.js are needed for the api docs page
+Rails.application.config.assets.precompile += %w[admin.css prism.css prism.js]
