d105e68dae289c5f0ccd3372cd6be2df3b524270
Vaclav Bohac <bohac.v@gmail.com>
2019-10-07 12:13:26 +0200

    Add some fields

--- app/controllers/experimental/admin/users_controller.rb
+++ app/controllers/experimental/admin/users_controller.rb
@@ -48,13 +48,14 @@ class Experimental::Admin::UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.fetch(:user, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.fetch(:user, {}).permit(:first_name, :last_name, :email, roles: %i[id])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ApplicationRecord
   has_many :notifications, foreign_key: "recipient_id"
   has_and_belongs_to_many :sources, join_table: "sources_experts"
 
+  validates :email, presence: true
+
   has_one_attached :avatar
 
   def full_name
