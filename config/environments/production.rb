9471593ab3c59e1c8e24fad2ad93527458dcc5b1
Jan Vlcek <vlki@vlki.cz>
2019-11-05 09:40:53 +0100

    Force https in production (#467)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  # Force https in all generated absolute urls
+  config.action_controller.default_url_options = { protocol: "https" }
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
