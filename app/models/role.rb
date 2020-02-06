e990bb4899a6ad222247a660d8f977f633909609
Vaclav Bohac <bohac.v@gmail.com>
2020-02-06 15:38:10 +0100

    Allow social media managers to edit articles (#496)

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,71 +1,56 @@
 # frozen_string_literal: true
 
-EXPERT_PERMISSIONS = [
-  "articles:view",
-  "articles:edit",
-
-  "availability:view",
-
-  "bodies:view",
-  "bodies:edit",
-
-  "images:view",
-  "images:add",
-  "images:delete",
-
-  "media:view",
-  "media:edit",
-
-  "media-personalities:view",
-  "media-personalities:edit",
-
-  "menu:view",
-
-  "pages:view",
-  "pages:edit",
-
-  "sources:view",
-  "sources:edit",
-
-  "speakers:view",
-  "speakers:edit",
-
-  "statements:add",
-  "statements:edit", # Allows editing of everything in statement
-  "statements:edit-as-evaluator", # Allows editing in being_evaluated status by evaluator
-  "statements:edit-as-proofreader", # Allows editing only texts and changing status to approved, for proofreaders
-  "statements:sort",
-  "statements:view-unapproved-evaluation",
-  "statements:view-evaluation-as-evaluator",
-  "statements:comments:add",
-  "statements:delete",
-
-  "tags:view",
-
-  "users:view",
-  "users:edit",
-
-  "visualizations:view",
+EXPERT_PERMISSIONS = %w[
+  articles:view
+  articles:edit
+  availability:view
+  bodies:view
+  bodies:edit
+  images:view
+  images:add
+  images:delete
+  media:view
+  media:edit
+  media-personalities:view
+  media-personalities:edit
+  menu:view
+  pages:view
+  pages:edit
+  sources:view
+  sources:edit
+  speakers:view
+  speakers:edit
+  statements:add
+  statements:edit
+  statements:edit-as-evaluator
+  statements:edit-as-proofreader
+  statements:sort
+  statements:view-unapproved-evaluation
+  statements:view-evaluation-as-evaluator
+  statements:comments:add
+  statements:delete
+  tags:view
+  users:view
+  users:edit
+  visualizations:view
 ]
 
-ADMIN_PERMISSIONS = EXPERT_PERMISSIONS + [
-  "admin:become-another-user"
-]
+ADMIN_PERMISSIONS = EXPERT_PERMISSIONS + %w[admin:become-another-user]
 
-INTERN_PERMISSIONS = [
-  "availability:view",
-  "bodies:view",
-  "images:view",
-  "images:add",
-  "media:view",
-  "media-personalities:view",
-  "sources:view",
-  "speakers:view",
-  "statements:edit-as-evaluator",
-  "statements:view-evaluation-as-evaluator",
-  "statements:comments:add",
-  "tags:view",
-  "visualizations:view",
+INTERN_PERMISSIONS = %w[
+  availability:view
+  bodies:view
+  images:view
+  images:add
+  media:view
+  media-personalities:view
+  sources:view
+  speakers:view
+  statements:edit-as-evaluator
+  statements:view-evaluation-as-evaluator
+  statements:comments:add
+  tags:view
+  visualizations:view
 ]
 
 class Role < ApplicationRecord
@@ -80,33 +65,41 @@ class Role < ApplicationRecord
   def permissions
     # Hardcoded now, can be turned into dynamic permissions assigning if needed
     case key
-    when ADMIN then ADMIN_PERMISSIONS
-    when EXPERT then EXPERT_PERMISSIONS
-    when SOCIAL_MEDIA_MANAGER then INTERN_PERMISSIONS + [
-        "articles:view",
-        "menu:view",
-        "pages:view",
-        "statements:view-unapproved-evaluation",
-        "users:view",
+    when ADMIN
+      ADMIN_PERMISSIONS
+    when EXPERT
+      EXPERT_PERMISSIONS
+    when SOCIAL_MEDIA_MANAGER
+      INTERN_PERMISSIONS +
+        %w[
+          articles:view
+          articles:edit
+          menu:view
+          pages:view
+          statements:view-unapproved-evaluation
+          users:view
+        ]
+    when PROOFREADER
+      %w[
+        articles:view
+        availability:view
+        bodies:view
+        images:view
+        images:add
+        media:view
+        media-personalities:view
+        sources:view
+        speakers:view
+        statements:edit-as-proofreader
+        statements:view-unapproved-evaluation
+        statements:comments:add
+        tags:view
+        visualizations:view
       ]
-    when PROOFREADER then [
-        "articles:view",
-        "availability:view",
-        "bodies:view",
-        "images:view",
-        "images:add",
-        "media:view",
-        "media-personalities:view",
-        "sources:view",
-        "speakers:view",
-        "statements:edit-as-proofreader",
-        "statements:view-unapproved-evaluation",
-        "statements:comments:add",
-        "tags:view",
-        "visualizations:view",
-      ]
-    when INTERN then INTERN_PERMISSIONS
-    else raise Exception.new("Permissions for role #{key} have not been implemented yet")
+    when INTERN
+      INTERN_PERMISSIONS
+    else
+      raise Exception.new("Permissions for role #{key} have not been implemented yet")
     end
   end
 end
