143f0675bacc47b0a13611d4180c9aa27fe99b99
Jan Vlcek <vlki@vlki.cz>
2020-07-24 14:37:00 +0200

    Update server IP in refresh dump script

--- scripts/refresh_production_dump.sh
+++ scripts/refresh_production_dump.sh
@@ -22,13 +22,13 @@ do
 done
 
 echo "[1/2] Refreshing production db dump..."
-ssh rails@demagog.cz '~/scripts/dump_production_db.sh'
-scp rails@demagog.cz:/tmp/demagog_production_db.zip /tmp/
+ssh rails@188.226.140.217 '~/scripts/dump_production_db.sh'
+scp rails@188.226.140.217:/tmp/demagog_production_db.zip /tmp/
 
 if $DATABASE_ONLY; then
   echo "[2/2] Skipping refreshing of production active storage blobs dump"
 else
   echo "[2/2] Refreshing production active storage blobs dump... (should take about 15 minutes)"
-  ssh rails@demagog.cz '~/scripts/dump_production_active_storage_blobs.sh'
-  scp rails@demagog.cz:/tmp/demagog_production_active_storage_blobs.zip /tmp/
+  ssh rails@188.226.140.217 '~/scripts/dump_production_active_storage_blobs.sh'
+  scp rails@188.226.140.217:/tmp/demagog_production_active_storage_blobs.zip /tmp/
 fi
