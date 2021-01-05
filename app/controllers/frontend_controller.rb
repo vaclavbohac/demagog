81912dc957bac4f9654b077aff92ccb3db93c853
Vaclav Bohac <bohac.v@gmail.com>
2021-01-05 21:24:08 +0100

    Enable 10m cache control on all FE controllers (#573)

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 class FrontendController < ApplicationController
+  before_action :set_public_expiration
+
   protected
     def set_locale
       # Whole app is using English except frontend which we want to
       # have with proper Czech localized dates and pagination
       I18n.locale = :cs
+    end
+
+    def set_public_expiration
+      expires_in 10.minutes, public: true
     end
 end
