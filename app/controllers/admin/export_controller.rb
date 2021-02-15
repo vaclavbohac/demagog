4435e4d4d90c70895cbd2b1c045fe5084c0426fa
Jan Vlcek <vlki@vlki.cz>
2021-02-15 10:39:44 +0100

    Better evaluation stats (#586)

--- app/controllers/admin/export_controller.rb
+++ app/controllers/admin/export_controller.rb
@@ -34,6 +34,48 @@ class Admin::ExportController < ApplicationController
       .where("assessments.first_requested_proofreading_at IS NOT NULL")
       .where("assessments.first_approved_at IS NOT NULL")
 
+    @statements_stats = {}
+    @statements.each do |statement|
+      @statements_stats[statement.id] = {
+        evaluator_first_assigned_mins: ((statement.assessment.evaluator_first_assigned_at - statement.created_at) / 60).round,
+        evaluation_started_mins: ((statement.assessment.evaluation_started_at - statement.assessment.evaluator_first_assigned_at) / 60).round,
+        first_requested_approval_mins: ((statement.assessment.first_requested_approval_at - statement.assessment.evaluation_started_at) / 60).round,
+        first_requested_proofreading_mins: ((statement.assessment.first_requested_proofreading_at - statement.assessment.first_requested_approval_at) / 60).round,
+        first_approved_mins: ((statement.assessment.first_approved_at - statement.assessment.first_requested_proofreading_at) / 60).round
+      }
+    end
+
+    sources_data = {}
+    @statements.each do |statement|
+      unless sources_data.has_key?(statement.source.id)
+        sources_data[statement.source.id] = {
+          source: statement.source,
+          evaluator_first_assigned_mins: [],
+          evaluation_started_mins: [],
+          first_requested_approval_mins: [],
+          first_requested_proofreading_mins: [],
+          first_approved_mins: []
+        }
+      end
+
+      sources_data[statement.source.id][:evaluator_first_assigned_mins].push(@statements_stats[statement.id][:evaluator_first_assigned_mins])
+      sources_data[statement.source.id][:evaluation_started_mins].push(@statements_stats[statement.id][:evaluation_started_mins])
+      sources_data[statement.source.id][:first_requested_approval_mins].push(@statements_stats[statement.id][:first_requested_approval_mins])
+      sources_data[statement.source.id][:first_requested_proofreading_mins].push(@statements_stats[statement.id][:first_requested_proofreading_mins])
+      sources_data[statement.source.id][:first_approved_mins].push(@statements_stats[statement.id][:first_approved_mins])
+    end
+
+    @sources_stats = sources_data.values.map do |item|
+      {
+        source: item[:source],
+        evaluator_first_assigned_mins: item[:evaluator_first_assigned_mins].sum / item[:evaluator_first_assigned_mins].size.to_f,
+        evaluation_started_mins: item[:evaluation_started_mins].sum / item[:evaluation_started_mins].size.to_f,
+        first_requested_approval_mins: item[:first_requested_approval_mins].sum / item[:first_requested_approval_mins].size.to_f,
+        first_requested_proofreading_mins: item[:first_requested_proofreading_mins].sum / item[:first_requested_proofreading_mins].size.to_f,
+        first_approved_mins: item[:first_approved_mins].sum / item[:first_approved_mins].size.to_f
+      }
+    end
+
     respond_to do |format|
       format.xlsx {
         response.headers["Content-Disposition"] = 'attachment; filename="Demagog_cz - Výroky s čísly o délce ověřování.xlsx"'
